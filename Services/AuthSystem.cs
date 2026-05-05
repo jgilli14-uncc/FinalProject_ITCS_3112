02d0e92c330ecdf67f62a0415bdbca60eda12525
WilliamSitt <w12501250@gmail.com>
2026-05-05 12:22:19 -0400

    Modified IAuthSystem and AuthSystem
    IAuthSystem is complete as per the UML, AuthSystem is still incomplete

--- Services/AuthSystem.cs
+++ Services/AuthSystem.cs
@@ -1,6 +1,46 @@
 using FinalProject3112.Interfaces;
+using FinalProject3112.Models;
 
 public class AuthSystem : IAuthSystem
 {
-    
+    private static AuthSystem instance;
+    private int sessionID;
+
+    private AuthSystem()
+    {
+        sessionID = 0;
+    }
+
+    private static AuthSystem getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new AuthSystem();
+        }
+        return instance;
+    }
+
+    public bool Login(int userID, string password)
+    {
+        if (userID < 0)
+        {
+            Console.WriteLine("Invalid User ID");
+        }
+
+        if (string.IsNullOrEmpty(password))
+        {
+            Console.WriteLine("Invalid Password");
+        }
+        return true;
+    }
+
+    public void Logout()
+    {
+        
+    }
+
+    public AuthLevel Verify(int userID)
+    {
+        
+    }
 }
